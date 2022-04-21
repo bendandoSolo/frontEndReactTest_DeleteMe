a43a713cd931555981c8bde3cadb4015b0779077
ben roberts <bendando@hotmail.co.uk>
2022-04-21 11:06:23 -0500

    get value from input element , NB: Formik could be removed depending on time

--- src/components/movie-list/index.js
+++ src/components/movie-list/index.js
@@ -1,13 +1,23 @@
-import React  from "react";
+import React, {useRef}  from "react";
 import "./index.css";
+import { Formik } from 'formik'; //not necessary as input box is actually a number so some validation occurs.
 
 function MovieList() {
+  
+  const inputElement = useRef();
+
+  const Search = () => {
+    let year = inputElement.current.value;
+    alert(year);
+  }
+
+
 
   return (
     <div className="layout-column align-items-center mt-50">
       <section className="layout-row align-items-center justify-content-center">
-        <input type="number" className="large" placeholder="Enter Year eg 2015" data-testid="app-input"/>
-        <button className="" data-testid="submit-button">Search</button>
+        <input ref={inputElement} type="number" className="large" placeholder="Enter Year eg 2015" data-testid="app-input"/>
+        <button className="" data-testid="submit-button" onClick={Search}>Search</button>
       </section>
 
       <ul className="mt-50 styled" data-testid="movieList">
